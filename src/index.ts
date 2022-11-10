07aaaa886627d2c3a17054bd8f8cf75b7b4f6879
unknown <adeelnasirkbw@gmail.com>
2022-11-10 11:55:06 -0800

    main index file configured

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,34 @@
-import { Post } from './entities/Post';
+import 'reflect-metadata';
+import { PostResolver } from './resolvers/post';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import config from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HealthCheck } from './resolvers/health-check';
+import { UserResolver } from './resolvers/user';
 
 const main = async () => {
   const orm = await MikroORM.init(config);
   const emFork = orm.em.fork();
   await orm.getMigrator().up();
 
-  //   const post = emFork.create(Post, { title: 'hello' });
-  //   await emFork.persistAndFlush(post);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HealthCheck, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: () => ({ emFork }),
+  });
 
-  const posts = await emFork.find(Post, {});
-  console.log(posts);
+  const app = express();
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(1337, () => {
+    console.log(`Server is listening on port 1337`);
+  });
 };
 
 main();
