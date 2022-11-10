a73e4a767cd5d54cf5344c6affb5c528131fc3e7
unknown <adeelnasirkbw@gmail.com>
2022-11-10 11:53:48 -0800

    user entity postgress table created

---
+++ src/entities/User.ts
@@ -0,0 +1,22 @@
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { ObjectType, Field } from 'type-graphql';
+
+@ObjectType()
+@Entity()
+export class User {
+  @Field()
+  @PrimaryKey()
+  id!: number;
+
+  @Field()
+  @Property({ type: 'text' })
+  name!: string;
+
+  @Field()
+  @Property({ type: 'text', unique: true, index: true })
+  email!: string;
+
+  @Field()
+  @Property({ type: 'text' })
+  password!: string;
+}
